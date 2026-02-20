89ef8f286c1baa69bb0871b642330d37499f01cd
migi0401 <migi111@naver.com>
2026-02-20 17:16:52 +0900

    chore: swagger 의존성 추가

--- src/main/java/NetZero/controller/StepController.java
+++ src/main/java/NetZero/controller/StepController.java
@@ -3,10 +3,7 @@ package NetZero.controller;
 import NetZero.dto.StepRequest;
 import NetZero.service.StepService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 import java.util.Map;
